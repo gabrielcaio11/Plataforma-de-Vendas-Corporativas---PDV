bf5dcdbd50b8dbc4550b7b4c9409c27181416e46
gabrielcaio11 <gabrielcaio848@gmail.com>
2026-04-07 19:14:48 -0300

    add validation annotations to LoginRequest and RegisterRequest for improved input validation

--- src/main/java/br/com/gabrielcaio/pdv/controller/dto/request/LoginRequest.java
+++ src/main/java/br/com/gabrielcaio/pdv/controller/dto/request/LoginRequest.java
@@ -1,5 +1,10 @@
 package br.com.gabrielcaio.pdv.controller.dto.request;
 
-public record LoginRequest(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-}
+public record LoginRequest(
+    @NotBlank(message = "Email não pode ser null") @Email String email,
+    @NotBlank @Size(min = 8,message = "Senha com no mínimo 8 caracteres") String password
+) {}
--- src/main/java/br/com/gabrielcaio/pdv/controller/dto/request/RegisterRequest.java
+++ src/main/java/br/com/gabrielcaio/pdv/controller/dto/request/RegisterRequest.java
@@ -1,8 +1,12 @@
 package br.com.gabrielcaio.pdv.controller.dto.request;
 
 import br.com.gabrielcaio.pdv.domain.UserRole;
+import jakarta.validation.constraints.NotBlank;
 
-public record RegisterRequest(String name, String email, String password, UserRole role,
-                              Long company_id) {
-
-}
+public record RegisterRequest(
+    @NotBlank(message = "Name não pode ser blank") String name,
+    @NotBlank(message = "Email não pode ser blank") String email,
+    @NotBlank(message = "Password não pode ser blank") String password,
+    @NotBlank(message = "Role não pode ser blank") UserRole role,
+    Long company_id
+) {}
