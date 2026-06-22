ccfb06bb6826ba5e79f894ca06f97071c191467c
gabrielcaio11 <gabrielcaio848@gmail.com>
2026-06-22 10:51:33 -0300

    feat: add base integration and repository test classes with Testcontainers support

---
+++ src/test/java/br/com/gabrielcaio/pdv/integration/base/BaseTestContainers.java
@@ -0,0 +1,24 @@
+package br.com.gabrielcaio.pdv.integration.base;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class BaseTestContainers {
+
+  @Container
+  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+      .withDatabaseName("test")
+      .withUsername("test")
+      .withPassword("test");
+
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+  }
+}
